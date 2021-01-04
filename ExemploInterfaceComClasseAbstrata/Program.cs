04f95c3b2918a3215538fa46cd5b06ff474d37d3
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2021-01-04 20:12:49 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploInterfaceComClasseAbstrata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploInterfaceComClasseAbstrata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
